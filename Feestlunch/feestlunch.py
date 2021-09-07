96db6fe2f06c5602211457b71c94c1d5813128b1
LaraMol <99068065@mydavinci.nl>
2021-09-07 07:55:34 +0200

    werken-met-gegevens

---
+++ Feestlunch/feestlunch.py
@@ -0,0 +1,11 @@
+
+
+
+croissantjes = 17
+stokbroden = 2
+croissantjesprijs = 0.39
+stokbrodenprijs = 2.78
+kortingsbonnen = 3
+kortingsbonnenprijs = 0.50
+
+print(croissantjes + stokbroden * (croissantjesprijs + stokbrodenprijs) - kortingsbonnen + kortingsbonnenprijs)
---
+++ begin opdracht/speelhal.py
@@ -0,0 +1,18 @@
+croissantjes = 0.39
+stokbroden  = 2.78
+kortingsbonnen = 0.50
+toegangsticket = 7.45
+VIPVR_Gameseat = 0.37
+
+calculate = 0.39 * 17 + 2 * 2.78
+
+print (str(calculate ) + ' Nu spreken we nog over de croissantjes en stokbroden.')
+
+calculate = 0.50 * 3
+print (str(calculate)+ ' kortingsbonnen')
+
+calculate = 0.37 * 9 + 7.45 * 3
+print (str(calculate)+ ' Toegangsticket + VIP VIPVR_Gameseat')
+
+calculate = 12.19 + 25.68 - 1.50 
+print (str(calculate)+ ' totale wat betaalt moet worden')
